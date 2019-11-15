694b1d5f9f6d4a929c232372e31b3fd1c8fe3695
myTerminal <team.terminal@gmail.com>
2019-11-15 22:11:16 +0000

    combine multiple files into 'config-misc.el'

--- .emacs.d/ample-emacs/config-misc.el
+++ .emacs.d/ample-emacs/config-misc.el
@@ -1,25 +1,43 @@
-;;Disable splash message, start *scratch* buffer by default
-(setq initial-buffer-choice 
+;;Startup
+(setq initial-buffer-choice
       t)
-(setq initial-scratch-message 
+(setq initial-scratch-message
       "")
 
-;;Delete lines along with carriage returns
+;;Interface
+(setq frame-title-format
+      "%b - ample-emacs")
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(ido-mode t)
+(load-theme 'wombat
+            t)
+(custom-set-faces
+ '(default ((t (:height 120)))))
+
+;;Text editing
+(electric-pair-mode)
+(show-paren-mode)
+(global-hl-line-mode -1)
+(setq-default indent-tabs-mode
+              nil)
+(set-default 'cursor-type
+             'hbar)
 (setq kill-whole-line
       t)
+(prefer-coding-system 'utf-8-unix)
 
-;;Enforce spaces for indentation, instead of tabs
-(setq-default indent-tabs-mode 
-              nil)
+;;Package archives and 'customize'
+(setq custom-file
+      (concat ae/config-root
+              "custom.el"))
+(setq package-user-dir
+      (concat ae/config-root
+              "elpa"))
+(setq package--init-file-ensured
+      t)
 
-;;Enable ido-mode
-(ido-mode t)
-
-;;Enable show-paren-mode
-(show-paren-mode)
-
-;;Enable winner-mode
+;;Misc
 (winner-mode t)
-
-;;Enable windmove
 (windmove-default-keybindings)
