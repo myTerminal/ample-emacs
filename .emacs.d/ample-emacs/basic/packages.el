1b2bd461aeca79231d8cd0f93b83f2a2e3a7a567
myTerminal <team.terminal@gmail.com>
2021-03-05 23:16:00 -0600

    split: packages.el

---
+++ .emacs.d/ample-emacs/basic/packages.el
@@ -0,0 +1,29 @@
+;;Set paths for standalone packages
+(add-to-list 'load-path
+             (concat ae/config-root
+                     "standalone-packages/multiple-cursors"))
+(add-to-list 'load-path
+             (concat ae/config-root
+                     "standalone-packages/ivy"))
+(add-to-list 'load-path
+             (concat ae/config-root
+                     "standalone-packages/which-key"))
+
+;;Load multiple-cursors
+(require 'multiple-cursors)
+
+;;Load ivy, counsel and swiper
+(require 'ivy)
+(require 'counsel)
+(require 'swiper)
+
+;;Start ivy
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers
+      t)
+
+;;Load which-key
+(require 'which-key)
+
+;;Start which-key-mode
+(which-key-mode)
