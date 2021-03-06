eeeb4a820f5cf4697a4f6b5f40407f8ad11249b8
myTerminal <team.terminal@gmail.com>
2021-03-05 22:46:37 -0600

    move config files down by one level

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,7 +2,7 @@
   (file-name-directory load-file-name))
 (load (expand-file-name "ample-emacs/core"
 			ae/config-root))
-(ae/load-file "config-misc")
-(ae/load-file "config-packages")
-(ae/load-file "config-key-bindings")
+(ae/load-file "standard/misc")
+(ae/load-file "standard/packages")
+(ae/load-file "standard/key-bindings")
 (ae/print-startup-message)
