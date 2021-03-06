8910e467768054d2a76c1a6b8165e2de136bc405
myTerminal <team.terminal@gmail.com>
2021-03-05 22:59:34 -0600

    set up foundation for basic <> standard split

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,27 @@
+;; Initialize root
 (defvar ae/config-root
   (file-name-directory load-file-name))
-(load (expand-file-name "ample-emacs/core"
-			ae/config-root))
-(ae/load-file "standard/misc")
-(ae/load-file "standard/packages")
-(ae/load-file "standard/key-bindings")
-(ae/print-startup-message)
+
+(defun ae/start-basic ()
+  "Loads basic configs"
+  (ae/load-file "basic/misc")
+  (ae/load-file "basic/packages")
+  (ae/load-file "basic/key-bindings"))
+
+(defun ae/start-standard ()
+  "Loads standard configs"
+  (ae/load-file "standard/misc")
+  (ae/load-file "standard/packages")
+  (ae/load-file "standard/key-bindings"))
+
+(defun ae/start ()
+  "Loads core and then conditionally loads configs"
+  (load (expand-file-name "ample-emacs/core"
+			              ae/config-root))
+  (ae/start-basic)
+  (if (display-graphic-p)
+      (ae/start-standard))
+  (ae/print-startup-message))
+
+;; Start
+(ae/start)
