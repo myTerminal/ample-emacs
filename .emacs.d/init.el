ed712466702babe73eeb792ac1ac075dee660131
myTerminal <team.terminal@gmail.com>
2021-07-14 14:23:13 -0500

    reduce private functions

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,27 +1,25 @@
+(require 'cl-lib)
+
 ;; Initialize root
 (defvar ae/config-root
   (file-name-directory load-file-name))
 
-(defun ae/start-basic ()
-  "Loads basic configs."
-  (ae/load-file "basic/misc")
-  (ae/load-file "basic/packages")
-  (ae/load-file "basic/key-bindings"))
-
-(defun ae/start-standard ()
-  "Loads standard configs."
-  (ae/load-file "standard/misc")
-  (ae/load-file "standard/packages")
-  (ae/load-file "standard/key-bindings"))
-
 (defun ae/start ()
   "Loads core and then conditionally loads configs."
-  (load (expand-file-name "ample-emacs/core"
-			              ae/config-root))
-  (ae/start-basic)
-  (if (display-graphic-p)
-      (ae/start-standard))
-  (ae/print-startup-message))
+  (cl-flet* ((start-basic ()
+                          (ae/load-file "basic/misc")
+                          (ae/load-file "basic/packages")
+                          (ae/load-file "basic/key-bindings"))
+             (start-standard ()
+                            (ae/load-file "standard/misc")
+                            (ae/load-file "standard/packages")
+                            (ae/load-file "standard/key-bindings")))
+    (load (expand-file-name "ample-emacs/core"
+			                ae/config-root))
+    (start-basic)
+    (if (display-graphic-p)
+        (start-standard))
+    (ae/print-startup-message)))
 
 ;; Start
 (ae/start)
